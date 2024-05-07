1fec22844ab71a7f984a6ba98750621610b4de74
Celeste040205 <inu-kun_2005@hotmail.com>
2024-05-07 12:57:24 -0500

    clase

---
+++ java/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+         int a;
+        Buclefor buclefor;
+
+        //instancias new
+        buclefor = new Buclefor();
+        buclefor.SignoAlterno();
+    }
+}
