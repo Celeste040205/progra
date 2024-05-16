a9f5a4693cc64c5bd2e4966cedf28cfa9d30e62f
Celeste040205 <inu-kun_2005@hotmail.com>
2024-05-15 23:17:05 -0500

    xd

--- prjClaseJava/src/App.java
+++ prjClaseJava/src/App.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class App {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         Usuario usuario = new Usuario();
 
         System.out.println(" ");
         usuario.ingresar();
         usuario.crearDino();
+        usuario.getEdad();
+
         System.out.println(" ");
     }
 }
--- prjClaseJava/src/Usuario.java
+++ prjClaseJava/src/Usuario.java
@@ -1,5 +1,15 @@
 public class Usuario {
     private String nombre;
+    private Integer edad;
+
+    public void setEdad(Integer edad) {
+        if (edad > 0)
+            this.edad = edad;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
 
     public void setNombre(String nombre){
         this.nombre = nombre.toUpperCase();
@@ -10,11 +20,15 @@ public class Usuario {
     }
 
     public Boolean ingresar() {
-        System.out.println("listo para el ingreso");
+        String nombreString;
+        System.out.println("Dime tu nombre: ");
+        nombreString = App.sc.nextLine();
+        setNombre(nombreString);
         return true;
     }
 
     public void crearDino() {
+        
         System.out.println("crear un dino");
         
     }
